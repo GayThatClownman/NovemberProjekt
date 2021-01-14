d44ff0b8d113479f5c4b67645ac26e3080a97654
GayThatClownman <60339604+GayThatClownman@users.noreply.github.com>
2021-01-14 16:29:51 +0100

    Start av deck-konstruktor + card metod

--- NovemberProjekt/Card.cs
+++ NovemberProjekt/Card.cs
@@ -5,7 +5,22 @@ namespace NovemberProjekt
     public class Card
     {
         private string suit;
-        private int number;
+        private int cardNumber;
+        private int cardValue;
 
+        public int GetValue()
+        {
+            if (cardNumber <= 10 && cardNumber >= 2)
+            {
+                cardValue = cardNumber;
+            }
+
+            if (cardNumber <= 13 && cardNumber >= 11)
+            {
+                cardValue = 11;
+            }
+
+            return cardValue;
+        }
     }
 }
--- NovemberProjekt/Deck.cs
+++ NovemberProjekt/Deck.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System;
 
 namespace NovemberProjekt
@@ -5,7 +6,15 @@ namespace NovemberProjekt
     public class Deck
     {
         public int cardCount;
+        public List<int> cardList;
 
-        
+        public Deck()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                
+                
+            }
+        }
     }
 }
